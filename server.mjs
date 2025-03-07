83f612f8f716ffec137ba2b21672fea0f716d78f
joshjones49 <joshjones200285@gmail.com>
2025-03-06 22:50:35 -0600

    Edited pool

--- server.mjs
+++ server.mjs
@@ -9,11 +9,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'rockets49',
-    database: process.env.DB_NAME || 'irontherapy',
+    host: process.env.HOST,
+    port: process.env.PORT || 5432,
+    user: process.env.USER,
+    password: process.env.PW,
+    database: process.env.DB_NAME,
 });
 //MIDDLEWARE===========================
 app.use(cors());
